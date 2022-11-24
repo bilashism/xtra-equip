39838128b6aa5d54243ea0d2539ce71455a82a4d
Md. Taifuzzaman Bilash <bilashism@gmail.com>
2022-11-24 17:17:50 +0600

    added Statistics

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import useTitle from "../../hooks/useTitle";
 import AdvertisedItems from "./AdvertisedItems";
 import Header from "./Header";
+import Statistics from "./Statistics";
 
 const Home = () => {
   useTitle("Home");
@@ -9,6 +10,7 @@ const Home = () => {
     <>
       <Header />
       <AdvertisedItems />
+      <Statistics />
     </>
   );
 };
