d3bce6549ff4aef221b36daf78ac484e5cf22de6
Md. Taifuzzaman Bilash <bilashism@gmail.com>
2022-11-28 11:28:41 +0600

    updated loader circle

--- src/components/ui/LoadingCircle.jsx
+++ src/components/ui/LoadingCircle.jsx
@@ -1,22 +1,29 @@
 import React from "react";
 
 /**
- * It returns a figure element with an image element inside of it.
- * @returns A React component.
+ * It returns a div with a loading circle svg and a span with the text "Loading..."
+ * @returns A React component that renders a loading circle.
  */
 const LoadingCircle = () => {
   return (
-    <figure className="text-center">
-      <img
-        src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' style='margin: auto; background: none; display: block; shape-rendering: auto;' width='78px' height='78px' viewBox='0 0 100 100' preserveAspectRatio='xMidYMid'%3E%3Ccircle cx='50' cy='50' fill='none' stroke='%23ebbc50' stroke-width='10' r='35' stroke-dasharray='164.93361431346415 56.97787143782138'%3E%3CanimateTransform attributeName='transform' type='rotate' repeatCount='indefinite' dur='1s' values='0 50 50;360 50 50' keyTimes='0;1'%3E%3C/animateTransform%3E%3C/circle%3E%3C/svg%3E"
-        alt="loading"
-        className="w-10 h-10 mx-auto"
-        width="40"
-        height="40"
-        decoding="async"
-        fetchpriority="low"
-      />
-    </figure>
+    <div role="status" className="mx-auto py-4">
+      <svg
+        aria-hidden="true"
+        class="mr-2 w-8 h-8 text-indigo-200 animate-spin fill-blue-600"
+        viewBox="0 0 100 101"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+          fill="currentColor"
+        />
+        <path
+          d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+          className="fill-indigo-600"
+        />
+      </svg>
+      <span class="sr-only">Loading...</span>
+    </div>
   );
 };
 
